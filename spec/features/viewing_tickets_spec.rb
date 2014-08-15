9557fd89e375236437294f8a27e9a8e11d3a7a70
Maroo-b <marouen.it.technology@gmail.com>
2014-08-15 15:32:34 +0100

    associate users to tickets

--- spec/features/viewing_tickets_spec.rb
+++ spec/features/viewing_tickets_spec.rb
@@ -3,16 +3,14 @@ feature "Viewing tickets" do
   before do
     textmate_2 = FactoryGirl.create(:project,
     name: "TextMate 2")
-    FactoryGirl.create(:ticket,
-    project: textmate_2,
-    title: "Make it shiny!",
-    description: "Gradients! Starbursts! Oh my!")
-    internet_explorer = FactoryGirl.create(:project,
-    name: "Internet Explorer")
-    FactoryGirl.create(:ticket,
-    project: internet_explorer,
-    title: "Standards compliance",
-    description: "Isn't a joke.")
+    user = FactoryGirl.create(:user)
+ticket = FactoryGirl.create(:ticket,
+project: textmate_2,
+title: "Make it shiny!",
+description: "Gradients! Starbursts! Oh my!")
+ticket.update(user: user)
+
+  
     visit '/'
   end
   scenario "Viewing tickets for a given project" do
