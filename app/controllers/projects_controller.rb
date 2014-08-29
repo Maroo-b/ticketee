493c7a13cc466f90e0a852337dccb45e57602cd9
Maroo-b <marouen.it.technology@gmail.com>
2014-08-29 22:00:23 +0100

    Add the ability to create users

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -64,12 +64,6 @@ class ProjectsController < ApplicationController
 end
 
 
-  def authorize_admin!
-    require_signin!
-    unless current_user.admin?
-       flash[:alert] = "You must be an admin to do that."
-        redirect_to root_path
-    end
-  end
+  
 
 end
